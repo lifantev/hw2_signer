20823766fe1f4b7b6eeec20f6abe609be7385bb4
lifantev <lifantev.da@phystech.edu>
2022-11-13 16:37:18 +0300

    execPipeline func

--- signer.go
+++ signer.go
@@ -1,7 +1,25 @@
 package main
 
+import "sync"
+
 func ExecutePipeline(jobs ...job) {
 
+	chans := make([]chan any, len(jobs)+1)
+	for i := range chans {
+		chans[i] = make(chan any)
+	}
+
+	wg := &sync.WaitGroup{}
+	for i, j := range jobs {
+		wg.Add(1)
+		go func(in, out chan any, j job) {
+			defer wg.Done()
+			j(in, out)
+			close(out)
+		}(chans[i], chans[i+1], j)
+	}
+
+	wg.Wait()
 }
 
 func SingleHash(in, out chan any) {
