ca20795526e1a763b3039fb74259a09ea73dcaa5
lifantev <lifantev.da@phystech.edu>
2022-11-20 18:13:44 +0300

    impl all funcs

--- signer.go
+++ signer.go
@@ -1,6 +1,11 @@
 package main
 
-import "sync"
+import (
+	"fmt"
+	"sort"
+	"strings"
+	"sync"
+)
 
 func ExecutePipeline(jobs ...job) {
 	wg := &sync.WaitGroup{}
@@ -23,13 +28,68 @@ func ExecutePipeline(jobs ...job) {
 }
 
 func SingleHash(in, out chan any) {
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 
+	for d := range in {
+		wg.Add(1)
+
+		go func(in any, out chan any, wg *sync.WaitGroup, mu *sync.Mutex) {
+			defer wg.Done()
+
+			data := fmt.Sprintf("%v", in)
+			mu.Lock()
+			md5Data := DataSignerMd5(data)
+			mu.Unlock()
+
+			crc32 := make(chan string)
+			go func(s string, out chan string) {
+				out <- DataSignerCrc32(s)
+			}(data, crc32)
+
+			out <- (<-crc32 + "~" + DataSignerCrc32(md5Data))
+		}(d, out, wg, mu)
+	}
+
+	wg.Wait()
 }
 
 func MultiHash(in, out chan any) {
+	wg := &sync.WaitGroup{}
 
+	for d := range in {
+		wg.Add(1)
+
+		go func(in any) {
+			defer wg.Done()
+
+			iWg := &sync.WaitGroup{}
+			mhs := make([]string, 6)
+
+			for th := 0; th < 6; th++ {
+				iWg.Add(1)
+
+				go func(th int, in any, mhs []string) {
+					defer iWg.Done()
+
+					mhs[th] = DataSignerCrc32(fmt.Sprint(th) + fmt.Sprintf("%v", in))
+				}(th, in, mhs)
+			}
+
+			iWg.Wait()
+			out <- strings.Join(mhs, "")
+		}(d)
+	}
+
+	wg.Wait()
 }
 
 func CombineResults(in, out chan any) {
+	var res []string
+	for d := range in {
+		res = append(res, fmt.Sprintf("%v", d))
+	}
 
+	sort.Strings(res)
+	out <- strings.Join(res, "_")
 }
